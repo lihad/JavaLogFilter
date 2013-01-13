02a10619a41a98b54593c92fc332bc3f6574c6b1
Lihad <kylecubed@comcast.net>
2013-01-12 18:29:11 -0800

    Added reload

--- src/lihad/JavaLogFilter/JavaLogFilter.java
+++ src/lihad/JavaLogFilter/JavaLogFilter.java
@@ -8,11 +8,17 @@ import java.util.logging.LogManager;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class JavaLogFilter extends JavaPlugin implements Filter {
 	public static Logger log;
+	protected static String PLUGIN_NAME = "JavaLogFilter";
+	protected static String header = "[" + PLUGIN_NAME + "] ";
 	private static List<String> toFilter = new LinkedList<String>();
 	public static FileConfiguration config;
 
@@ -43,5 +49,32 @@ public class JavaLogFilter extends JavaPlugin implements Filter {
 			return b;
 		}
 		return true;
+	}
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		Player player = (Player)sender;
+		if(cmd.getName().equalsIgnoreCase("filter") && player.isOp()){
+			if(args.length == 1 && args[0].equalsIgnoreCase("reload")){
+				reload();
+			}
+			return true;
+		}
+		return false;
+	}
+	private void reload(){
+		config = getConfig();
+		toFilter = config.getStringList("LogFilter");
+		info("Reloaded");
+	}
+	private static void info(String message){ 
+		log.info(header + ChatColor.WHITE + message);
+	}
+	private static void severe(String message){
+		log.severe(header + ChatColor.RED + message);
+	}
+	private static void warning(String message){
+		log.warning(header + ChatColor.YELLOW + message);
+	}
+	private static void log(java.util.logging.Level level, String message){
+		log.log(level, header + message);
 	}
 }
